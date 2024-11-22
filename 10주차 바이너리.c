069ea76cb60e567c81e08d0d2663371a3b2d1180
jang0521 <103359714+jang0521@users.noreply.github.com>
2024-11-22 19:10:21 +0900

    Add files via upload

---
+++ 10주차 바이너리.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define SIZE 100
+
+int main() {
+    char input[SIZE];
+    FILE* fp =NULL;
+    if ((fp=open("output.bin","wb"))==NULL){
+        printf("error...");
+        return 0;
+    }
+    gets(input);
+    fwrite(input,strlen(input),1,fp);
+    fclose(fp);
+    return 0;
+}
